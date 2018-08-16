4c4c23fe9bc79cdd578ba664f6bfe42e1eaf4b26
Stanislav Valasek <valasek@gmail.com>
2018-08-17 00:45:42 +0200

    binaries moved to binaries folder, spacing added

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 ECHO Compiling for MS Windows ...
 set GOOS=windows
 set GOARCH=amd64
-vgo build .\rajce-get.go
+vgo build -o .\binaries\rajce-get.exe .\rajce-get.go
 ECHO Compiling for Linux ...
 set GOOS=linux
 set GOARCH=amd64
-vgo build -o rajce-get.bin .\rajce-get.go
+vgo build -o .\binaries\rajce-get.bin .\rajce-get.go
 ECHO Compiling for MAC OS X ...
 set GOOS=darwin
 set GOARCH=amd64
-vgo build -o rajce-get.app .\rajce-get.go
+vgo build -o .\binaries\rajce-get.app .\rajce-get.go
--- rajce-get.go
+++ rajce-get.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"io"
 	"flag"
 	"fmt"
-	"net/http"
 	"github.com/PuerkitoBio/goquery"
+	"io"
+	"net/http"
 	"os"
-	"time"
 	"regexp"
+	"time"
 )
+
+type remoteFile struct {
+	url  string
+	file string
+}
 
 func main() {
 	fmt.Println("Starting ...")
@@ -17,35 +22,34 @@ func main() {
 	flag.Parse()
 
 	args := flag.Args()
-    if len(args) != 1 {
-        usage()
-    }
+	if len(args) != 1 {
+		usage()
+	}
+
 	//img31.rajce.idnes.cz/d3103/15/15400/15400849_2b8b5e522f4cde0cd7d89efe79cb0a63/images/MOV_0180.jpg
 	urlPattern, err := regexp.Compile(`(img[0-9]{2}.rajce.idnes.cz/.*/)(.*)`)
 	if err != nil {
 		fmt.Printf("There is a problem with your regexp.\n")
 		os.Exit(2)
 	}
-	type remoteFile struct {
-		url string
-		file string
-	}
+
 	var URLs []remoteFile
 	doc, err := goquery.NewDocument(flag.Args()[0])
 	if err != nil {
-        fmt.Println("There is a problem getting a page.", err)
+		fmt.Println("There is a problem getting a page.", err)
 		os.Exit(2)
 	}
+
 	downloaded := 0
 	doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
 		href, _ := item.Attr("href")
 		res := urlPattern.FindAllStringSubmatch(href, -1)
 		for i := range res {
-			//fmt.Println(res[i][1], res[i][2], "\n")
 			URLs = append(URLs, remoteFile{url: res[i][1], file: res[i][2]})
 			downloaded++
 		}
 	})
+
 	for _, remFile := range URLs {
 		downloaded, err := downloadFile(remFile.file, "HTTPS://"+remFile.url+"/"+remFile.file)
 		if err != nil {
@@ -65,19 +69,24 @@ func usage() {
 }
 
 func downloadFile(filePath string, url string) (downloaded int64, error error) {
+
 	out, err := os.Create(filePath)
+
 	if err != nil {
-     	return 0, err
+		return 0, err
 	}
 	defer out.Close()
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return 0, err
 	}
 	defer resp.Body.Close()
+
 	n, err := io.Copy(out, resp.Body)
 	if err != nil {
 		return 0, err
 	}
+
 	return n, nil
 }
